69b4dd8829bb4cea0989764eff523780f42b7334
Nikhil <ace.nikchauhan@gmail.com>
2024-08-25 17:11:49 +0530

    added user-repository and user-service

---
+++ src/repository/user-repository.js
@@ -0,0 +1,19 @@
+import User from "../models/user";
+import CrudRepository from "./crud-repository";
+
+class UserRepository extends CrudRepository {
+  constructor() {
+    super(User);
+  }
+
+  async findBy(data) {
+    try {
+      const response = await User.findOne(data);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default UserRepository;
---
+++ src/services/user-service.js
@@ -0,0 +1,40 @@
+import UserRepository from "../repository/user-repository";
+
+class UserService {
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
+
+  async signup(data) {
+    try {
+      const user = await this.userRepository.create(data);
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getUserByEmail(email) {
+    try {
+      const user = await this.userRepository.findBy({ email });
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async signin(data) {
+    try {
+      const user = await this.getUserByEmail(data.email);
+      if (!user) {
+        throw {
+          message: "No user found",
+        };
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default UserService;
