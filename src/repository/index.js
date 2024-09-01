cf252bec876999a8567db22fd878f204bc08bf38
Nikhil <ace.nikchauhan@gmail.com>
2024-09-01 07:43:17 +0530

    added comment and like repository

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,5 +1,13 @@
 import HashtagRepository from "./hashtag-repository.js";
 import UserRepository from "./user-repository.js";
 import TweetRepository from "./tweet-repository.js";
+import CommentRepository from "./comment-repository.js";
+import LikeRepository from "./like-repository.js";
 
-export { HashtagRepository, UserRepository, TweetRepository };
+export {
+  HashtagRepository,
+  UserRepository,
+  TweetRepository,
+  LikeRepository,
+  CommentRepository,
+};
